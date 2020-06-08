2b52da276e4e772799d26569172a883ad63eee10
Nikolaus Hauer <ic18b013@technikum-wien.com>
2020-06-08 21:45:27 +0200

    Add: Components ListSpells.vue and ListKinds.vue created. /list/spell and /list/kind now hold a table with all entries from the corresponding database.

---
+++ resources/js/components/ListKinds.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>List Kinds Component</h1>
+        <table class="table table-hover">
+            <tbody>
+            <tr>
+                <th>id</th>
+                <th>slug</th>
+                <th>name</th>
+                <th>description</th>
+                <th>created_at</th>
+                <th>updated_at</th>
+            </tr>
+            <tr v-for="kinds in listKinds" :key="kinds.id">
+                <td>{{kinds.id}}</td>
+                <td>{{kinds.slug}}</td>
+                <td>{{kinds.name}}</td>
+                <td>{{kinds.description}}</td>
+                <td>{{kinds.created_at}}</td>
+                <td>{{kinds.updated_at}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+          return {
+              listKinds: {}
+          }
+        },
+        created() {
+            axios.get('./list/kind')
+            .then(({data}) => this.listKinds = data);
+            console.log('List kinds created.')
+        }
+    };
+    methods: {
+
+    }
+</script>
---
+++ resources/js/components/ListSpells.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>List Spells Component</h1>
+        <table class="table table-hover">
+            <tbody>
+            <tr>
+                <th>id</th>
+                <th>slug</th>
+                <th>name</th>
+                <th>quote</th>
+                <th>description</th>
+                <th>kind_id</th>
+                <th>created_at</th>
+                <th>updated_at</th>
+            </tr>
+            <tr v-for="spells in listSpells" :key="spells.id">
+                <td>{{spells.id}}</td>
+                <td>{{spells.slug}}</td>
+                <td>{{spells.name}}</td>
+                <td>{{spells.quote}}</td>
+                <td>{{spells.description}}</td>
+                <td>{{spells.kind_id}}</td>
+                <td>{{spells.created_at}}</td>
+                <td>{{spells.updated_at}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+          return {
+              listSpells: {}
+          }
+        },
+        created() {
+            axios.get('./list/spell')
+            .then(({data}) => this.listSpells = data);
+            console.log('List spells created.')
+        }
+    };
+    methods: {
+
+    }
+</script>
