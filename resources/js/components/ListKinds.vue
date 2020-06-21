816cb5a1b84f9c9139d4057d9b8e9f4f8f547ee2
Nikolaus Hauer <ic18b013@technikum-wien.com>
2020-06-21 23:48:28 +0200

    Edit kinds läuft ebenso

--- resources/js/components/ListKinds.vue
+++ resources/js/components/ListKinds.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
         <div class="container is-fluid">
-            <h1 class="title is-3 is-spaced">List of all Kinds</h1>
-            <div class="table-container">
+            <div>
+                <h1 class="title is-3 is-spaced" >Edit the chosen Kind</h1>
+
+                <div class="columns is-multiline is-mobile">
+                    <div class="column is-two-thirds"><input v-model="name" class="input is-primary" type="text" key="kindSlug" placeholder="Kind Name"></div>
+                    <div class="column is-full"><textarea v-model="description" class="textarea is-primary" type="text" placeholder="Kind Description"></textarea></div>
+                </div>
+
+                <button class="button is-primary" type="submit" v-on:click="update(name, description, editKinds)">Submit</button>
+            </div>
+
+            <h1 class="title is-3 is-spaced" style="padding-top: 100px">List of all Kinds</h1>
+            <div class="table-container" >
                 <table class="table is-striped is-hoverable table-hover">
                     <thead>
                         <tr>
@@ -15,7 +26,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="kinds in listKinds" :key="kinds.id">
+                        <tr v-for="kinds in listKinds" :key="kinds.id" v-on:click="edit(kinds)">
                             <td>{{kinds.id}}</td>
                             <td>{{kinds.slug}}</td>
                             <td>{{kinds.name}}</td>
@@ -37,6 +48,9 @@
         },
         data() {
           return {
+              name: "",
+              description: "",
+              editKinds: {},
               listKinds: {}
           }
         },
@@ -44,9 +58,21 @@
             axios.get('./list/kind')
             .then(({data}) => this.listKinds = data);
             console.log('List kinds created.')
+        },
+
+        methods: {
+            edit(kinds) {
+                this.name = kinds.name
+                this.description = kinds.description
+                this.editKinds = kinds
+                console.log('Edit kinds called.')
+            },
+            update(name, description, editKinds) {
+                axios
+                    .put('/kind/'+ editKinds.slug, {name, description})
+                    .then(({data}) => this.editKinds = data);
+                console.log('Update kinds called.')
+            }
         }
     };
-    methods: {
-
-    }
 </script>
